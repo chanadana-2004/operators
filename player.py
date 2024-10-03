692caeecc9e8234252eaa0eb08bc3c1ada312808
chanadana-2004 <22hr1a3213@mtieat.org>
2024-10-03 14:03:44 +0530

    Update player.py

--- player.py
+++ player.py
@@ -32,3 +32,13 @@ pe=(e/ti)*100
 print("%.2f"%ps,"%.2f"%pb,"%.2f"%pe)
 '''
 50.00 10.00 40.00
+
+program:
+s = int(input())
+b = int(input())
+e = int(input())
+ti = s + b + e
+ps = (s / ti) * 100  
+pb = (b / ti) * 100  
+pe = (e / ti) * 100  
+print("%.2f" % ps, "%.2f" % pb, "%.2f" % pe)
